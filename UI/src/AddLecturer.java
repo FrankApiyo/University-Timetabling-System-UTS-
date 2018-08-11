8b8e812ef3d3e1baedff720275f244d82b31f420
Charles Bett <cbrianbet@outlook.com>
2018-08-11 12:48:15 +0300

    Some minor improvements

--- UI/src/AddLecturer.java
+++ UI/src/AddLecturer.java
@@ -1,7 +1,6 @@
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -10,7 +9,6 @@ import javafx.stage.Stage;
 import timetable.DbDriver;
 import timetable.Lecturer;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -42,6 +40,8 @@ public class AddLecturer implements Initializable {
         addRoomWindow.setScene(backButScene);
         addRoomWindow.show();
     }
+    
+    
     public void initialize(URL url, ResourceBundle rb) {
 
     }
--- UI/src/HomeV2Controller.java
+++ UI/src/HomeV2Controller.java
@@ -8,8 +8,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -22,6 +24,21 @@ public class HomeV2Controller implements Initializable {
     
     @FXML
     private BorderPane border_pane;
+    @FXML
+    private Button btn;
+    @FXML
+    private Button btn1;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Button btn3;
+    @FXML
+    private Button btn4;
+    @FXML
+    private Button btn5;
+    @FXML
+    private Button btn6;
+    
     
     /**
      * Initializes the controller class.
@@ -30,12 +47,21 @@ public class HomeV2Controller implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
          
     }    
-
+    /**
+     * 
+     * @param event
+     * @throws IOException 
+     */
     @FXML
     private void show_request_req(MouseEvent event) throws IOException {
         DynamicView.loadBorderCenter(border_pane, "reqRequest.fxml");
     }
-
+    
+    /**
+     * 
+     * @param event
+     * @throws IOException 
+     */
     @FXML
     private void showCheckReq(MouseEvent event) throws IOException {
         DynamicView.loadBorderCenter(border_pane, "checkReq.fxml");
@@ -69,10 +95,77 @@ public class HomeV2Controller implements Initializable {
         addRoomWindow.setScene(new Scene(root));
         addRoomWindow.show();
     }
-    
+    /**
+     * 
+     * @throws IOException 
+     */
     @FXML
-    private void showIEO() throws IOException{
+    private void showIEO(MouseEvent event) throws IOException{
         DynamicView.loadBorderCenter(border_pane, "ieo.fxml");
     }
+    
+    
+
+    @FXML
+    private void menuDecoratorE(MouseEvent event) {
+        btn.setStyle("-fx-background-color:#43a047");
+    }
+    @FXML
+    private void menuDecoratorE1(MouseEvent event) {
+        btn1.setStyle("-fx-background-color:#43a047");
+    }
+    @FXML
+    private void menuDecoratorE2(MouseEvent event) {
+        btn2.setStyle("-fx-background-color:#43a047");
+    }
+    @FXML
+    private void menuDecoratorE3(MouseEvent event) {
+        btn3.setStyle("-fx-background-color:#43a047");
+    }
+    @FXML
+    private void menuDecoratorE4(MouseEvent event) {
+        btn4.setStyle("-fx-background-color:#43a047");
+    }
+    @FXML
+    private void menuDecoratorE5(MouseEvent event) {
+        btn5.setStyle("-fx-background-color:#43a047");
+    }
+    @FXML
+    private void menuDecoratorE6(MouseEvent event) {
+        btn6.setStyle("-fx-background-color:#43a047");
+    }
+    
+    
+    @FXML
+    private void menuDecoratorX(MouseEvent event) {
+        btn.setStyle("-fx-background-color:#ffffff");
+    }
+    
+    @FXML
+    private void menuDecoratorX1(MouseEvent event) {
+        btn1.setStyle("-fx-background-color:#ffffff");
+    }
+    @FXML
+    private void menuDecoratorX2(MouseEvent event) {
+        btn2.setStyle("-fx-background-color:#ffffff");
+    }
+    @FXML
+    private void menuDecoratorX3(MouseEvent event) {
+        btn3.setStyle("-fx-background-color:#ffffff");
+    }
+    @FXML
+    private void menuDecoratorX4(MouseEvent event) {
+        btn4.setStyle("-fx-background-color:#ffffff");
+    }
+    @FXML
+    private void menuDecoratorX5(MouseEvent event) {
+        btn5.setStyle("-fx-background-color:#ffffff");
+    }
+    @FXML
+    private void menuDecoratorX6(MouseEvent event) {
+        btn6.setStyle("-fx-background-color:#ffffff");
+    }
+    
+    
     
 }
